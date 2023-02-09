0a2b75f2fc6916f7af4723f8b8ea71d25d4e55f9
Rhenan Silverio Teixeira <rhenancontato@gmail.com>
2023-02-09 14:50:49 -0300

    estrturacao do teste com os assets, logos, fonts e sons

--- pokemon/src/App.js
+++ pokemon/src/App.js
@@ -1,22 +1,63 @@
-import React from 'react'
-import {BrowserRouter as Router, Route} from 'react-router-dom'
-import {Nav, List, Detalhes} from './Component'
-import './App.css'
+import { BrowserRouter } from "react-router-dom";
+import Main from "./components/Main";
+import { useState, React } from "react";
+import logo from "./assets/items/pokedex.png";
+import { Howl } from "howler";
+import mainTheme from "./assets/sounds/mainTheme.mp3";
 
-const App = () => {
-    return (
-        <Router>
-            <Nav/>
-            <div className="App">
-                <Route exact path = '/'>
-                    <List />
-                </Route>
-                <Route exact path = '/pokemon/:id'>
-                    <Detalhes/>
-                </Route>
+function App() {
+  const [start, setStart] = useState(false);
+
+  var sound = {
+    overworld: new Howl({
+      src: mainTheme,
+      loop: true,
+    }),
+  };
+
+  const startPokedex = () => {
+    setStart(true);
+    if (!sound.overworld.playing()) {
+        sound.overworld.play();
+    }
+  };
+
+  return (
+    <BrowserRouter className="App">
+      {start ? (
+        <Main />
+      ) : (
+        <div className="start-menu">
+          <div className="red-background position-start-item">
+            <img src={logo} />
+          </div>
+          <div className="ball-middle"></div>
+          <div className="white-background position-start-item">
+            <div className="center" onClick={startPokedex}>
+              <button className="btn">
+                <svg
+                  width="180px"
+                  height="50px"
+                  viewBox="0 0 180 50"
+                  className="border"
+                >
+                  <polyline
+                    points="179,1 179,59 1,59 1,1 179,1"
+                    className="bg-line"
+                  />
+                  <polyline
+                    points="179,1 179,59 1,59 1,1 179,1"
+                    className="hl-line"
+                  />
+                </svg>
+                <span>START</span>
+              </button>
             </div>
-        </Router>
-    )
+          </div>
+        </div>
+      )}
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- pokemon/src/components/index.js
+++
@@ -1,3 +0,0 @@
-export {default as Nav } from './Nav/Nav'
-export {default as List } from './List/List'
-export {default as Detalhes } from './Detail/Detalhes'
